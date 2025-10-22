de2de50caf62fc35805c923ef7f7813a9c553833
avilesoff <joseaviles237@gmail.com>
2025-10-21 18:15:24 -0600

    chore: disable SW (unregister) + add vercel.json at root

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppWrapper from './App'; // ← cambio importante
+import AppWrapper from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
@@ -13,4 +14,6 @@ root.render(
 );
 
 reportWebVitals();
-serviceWorkerRegistration.register();
+
+// 🔧 Desactiva el Service Worker para evitar servir builds antiguas
+serviceWorkerRegistration.unregister();
